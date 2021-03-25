4afc6f687c1937b765c987bf80a2dcbc0e02be52
Eddie Anguiano <eddiesanguiano@gmail.com>
2021-03-24 19:36:36 -0700

    commit two

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-var document,
-	console;
+var document, console;
 
 window.setInterval(function () {
-	var date = new Date();
-	var hours = date.getHours();
-	var minutes = date.getMinutes();
-	var seconds = date.getSeconds();
-	var secondHand = document.querySelector(".second");
-	var minuteHand = document.querySelector(".minute");
-	var hourHand = document.querySelector(".hour");
+  var date = new Date();
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  var secondHand = document.querySelector(".second");
+  var minuteHand = document.querySelector(".minute");
+  var hourHand = document.querySelector(".hour");
 
-	secondHand.style.transform = `rotate(${seconds * 6 - 90}deg)`;
-	minuteHand.style.transform = `rotate(${minutes * 6 - 90}deg)`;
-	hourHand.style.transform = `rotate(${hours * 30 - 90}deg)`;
+  secondHand.style.transform = `rotate(${seconds * 6 - 90}deg)`;
+  minuteHand.style.transform = `rotate(${minutes * 6 - 90}deg)`;
+  hourHand.style.transform = `rotate(${hours * 30 - 90}deg)`;
 
-	console.log("actual time:", hours, minutes, seconds, "clock time:", hours);
+  console.log("actual time:", hours, minutes, seconds, "clock time:", hours);
 }, 1000);
+
+//commit two
